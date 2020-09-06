b8d8e42d1f8573ced39894637308450724e5a5a5
aawezk456 <aawezk456@gmail.com>
2020-09-06 12:40:10 +0530

    Update: Server

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const expressGraphQL = require('express-graphql');
+const schema = require('./schema.js');
+const cors = require('cors');
+
+const app = express();
+
+
+app.use(cors());
+
+app.use('/graphql', expressGraphQL({
+    schema:schema,
+    graphiql:true
+}));
+
+app.listen(4000, () => {
+    console.log('Server is running on port 4000..');
+});
